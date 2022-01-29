2def13f6b3b881c116454220e0abb1cb6f5005c6
daniel <daniel.dl14121@gmail.com>
2022-01-29 17:35:23 -0600

    nav terminado y img-icons agregados

---
+++ script.js
@@ -0,0 +1,6 @@
+const menu = document.getElementById('menu');
+const btn = document.getElementById('menu-btn');
+
+    btn.addEventListener('click', () => {
+        menu.classList.toggle('active');
+    });
