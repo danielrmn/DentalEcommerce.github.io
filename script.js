bcba4669111e289936bbb66a6f81f6151d0c73ae
daniel <daniel.dl14121@gmail.com>
2022-01-29 20:51:38 -0600

    aun falta arreglar el hero

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const menu = document.getElementById('menu');
 const btn = document.getElementById('menu-btn');
+// const img = document.getElementById('img-hero');
 
     btn.addEventListener('click', () => {
         menu.classList.toggle('active');
+        // img.style.position = 'relative';
     });
+
+
