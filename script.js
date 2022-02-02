4c0d31eea2dd4813c2df28f124773f5fdcd1b9db
daniel <daniel.dl14121@gmail.com>
2022-02-02 01:25:41 -0600

    solo falta el ultimo section

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 const menu = document.getElementById('menu');
 const btn = document.getElementById('menu-btn');
-// const img = document.getElementById('img-hero');
 
     btn.addEventListener('click', () => {
         menu.classList.toggle('active');
-        // img.style.position = 'relative';
     });
 
 
