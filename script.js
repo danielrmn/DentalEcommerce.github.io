3315937b997e336f3b3d766b4c8eaf3b09553007
daniel <daniel.dl14121@gmail.com>
2022-05-24 22:59:20 -0600

    estilos cambiados, falta agragar la pagina para comprar

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const menu = document.getElementById('menu');
 const btn = document.getElementById('menu-btn');
+const close = document.querySelector('.close');
+const span = document.querySelector('.menu-btn span')
 
-    btn.addEventListener('click', () => {
-        menu.classList.toggle('active');
-    });
-
+btn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    close.classList.toggle('active');
+    span.classList.toggle('active');
+})
 
